87668f97866b30fdfc281dd96b7932e3df6bb774
Amar <onlyaboutcoc@gmail.com>
2024-10-27 00:15:42 +0530

    asad

--- app/page.js
+++ app/page.js
@@ -1,101 +1,65 @@
 import Image from "next/image";
+import leftImage from "../image/left2.png";
+import rightImage from "../image/right2.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+    <header>
+        <nav className="flex justify-around items-center h-[3.5em] bg-gradient-to-r from-[#070736] to-[#08084E]">
+          <div className="text-xl ml-[-24rem] text-white">BudgetBliss</div>
+          <div className="flex items-end h-[3.5em] mr-[-5em]">
+          <div className="h-[3.5em] px-4 leading-[3.5em] hover:cursor-pointer hover:bg-[#320D7B]/50 hover:border hover:border-[#8908CF]/40">
+              <Link href="/login" className="text-white no-underline hover:text-[#7878DB]">
+                Login
+              </Link>
+            </div>
+            <div className="h-[3.5em] px-4 leading-[3.5em] hover:cursor-pointer hover:bg-[#320D7B]/50 hover:border hover:border-[#8908CF]/40">
+              <Link href="/signup" className="text-white no-underline hover:text-[#7878DB]">
+                Sign up
+              </Link>
+            </div>
+          </div>
+        </nav>
+        <Image src={rightImage} alt="BudgetBliss Logo" className="w-56 h-56" />
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      <main>
+        <section className="flex justify-around mt-8 first">
+          <div className="leftSection w-[30%] text-4xl mt-20 text-black">
+            Welcome to 
+            <span className="name text-[#329A39]"> BudgetBliss</span>
+            <div className="pb-5 text-lg font-medium text-gray-500 text">
+              Peace of mind, One budget at a time
+            </div>
+            <span id="element" className="text-[#3FDA49]">What can we do for you?</span>
+          </div>
+          <div className="rightSection w-[30%]">
+            <Image src={leftImage} alt="developer image" className="h-[55vh] w-[90%] mt-4 filter drop-shadow-[5px_5px_20px_rgba(130,232,48,0.797)]" />
+          </div>
+        </section>
+
+        <hr className="border-0 bg-[#7528E1] h-[0.07em] mx-[5em] mt-[5em]" />
+
+        <section className="second max-w-[80vw] mx-auto mt-4 flex justify-around mb-12">
+          <div className="w-[30%] h-[10em] bg-[#D7C144]/80 text-[#070736] rounded-lg"></div>
+          <div className="w-[30%] h-[10em] bg-[#D7C144]/80 text-[#070736] rounded-lg"></div>
+          <div className="w-[30%] h-[10em] bg-[#D7C144]/80 text-[#070736] rounded-lg"></div>
+        </section>
+
+        <footer className="mt-80 bg-gradient-to-r from-[#070736] to-[#08084E]">
+          <hr />
+          <div className="mt-12 text-center text-black copyright">
+            <div className="flex items-center justify-center gap-8 pages">
+              <a href="/terms" className="text-black">Conditions of Use</a>
+              <a href="/privacy" className="text-black">Privacy Notice</a>
+              <a href="/ads-choices" className="text-black">Your Ads Privacy Choices</a>
+            </div>
+            <div className="pb-4 mt-1 copyright-tag">© 1996-2024, budgetbliss.com, Inc. or its affiliates</div>
+          </div>
+        </footer>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
